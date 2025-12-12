d433b5f66b6fe71799ab0659208e195e101d12a1
Ruata Hmar <hvanlalruata511@gmail.com>
2025-12-12 18:48:50 +0530

    dashboard v1

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+
+
+export default function NavBar(){
+    return(
+        <div className="bg-[#E5E6E4] w-1/5 h-full p-6"> 
+                <h1 className="text-4xl">LinkLens</h1>
+                <ul>
+                    <li>Dashboard</li>
+                    <li>My Links</li>
+                </ul>
+        </div>
+    )
+}
--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,5 +1,31 @@
-
+import NavBar from "../components/navbar"
 
 export default function Dashboard(){
-    
+    return(
+    <>
+        <div className="flex flex-row h-screen w-screen bg-red ">
+            <NavBar/>
+            <div className="bg-[#F2F3F2] w-4/5 h-full p-10">
+                <h1 className="text-7xl font-bold">
+                    DashBoard
+                </h1>
+                <div className="flex flex-row ">
+                    <div className="m-10 p-8 h-40 w-50 border border-black flex flex-col rounded-lg">
+                        <h3 className="text-2xl">Total Links</h3>
+                        <h1 className="font-bold text-5xl mt-3">100</h1>
+                    </div>
+                    <div className="m-10 p-8 h-40 w-50 border border-black flex flex-col rounded-lg">
+                        <h3 className="text-2xl">Total Clicks</h3>
+                        <h1 className="font-bold text-5xl mt-3">100</h1>
+                    </div>
+                    <div className="m-10 p-8 h-40 w-55 border border-black flex flex-col rounded-lg">
+                        <h3 className="text-2xl">Unique Clicks</h3>
+                        <h1 className="font-bold text-5xl mt-3">100</h1>
+                    </div>
+                </div>
+                <button>Create New Link</button>
+            </div>
+        </div>
+    </>
+    )
 }
