7bb0b756d9d07bc1b3c4589408bfc3fed56c2702
Ruata Hmar <hvanlalruata511@gmail.com>
2026-01-16 14:59:25 +0530

    axios interscept updated at apiClient

--- client/src/utils/apiClients.js
+++ client/src/utils/apiClients.js
@@ -5,4 +5,51 @@ const api = axios.create({
     withCredentials: true
 });
 
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+    failedQueue.forEach(prom => {
+        if (error) prom.reject(error);
+        else prom.resolve(token);
+    });
+    failedQueue = [];
+};
+
+api.interceptors.response.use(
+    res => res,
+    async error => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest._retry) { //originalRequest._retry to prevent infinite loop
+            if (isRefreshing) {
+                return new Promise((resolve, reject) => {
+                    failedQueue.push({ resolve, reject });
+                }).then(() => api(originalRequest));
+            }
+
+            originalRequest._retry = true;
+            isRefreshing = true;
+
+            try {
+                await api.post("/auth/refresh");
+                processQueue(null);
+                return api(originalRequest);
+            } catch (err) {
+                processQueue(err, null);
+                // redirect to login
+                window.location.href = "/login";
+                return Promise.reject(err);
+            } finally {
+                isRefreshing = false;
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+
+
+
 export default api;
